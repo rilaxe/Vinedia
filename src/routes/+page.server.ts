fdaf589802369c30fbd5c07166cb12c9fe5c336f
Oparaiwu Silas <silas@pixelsgroup.ng>
2024-03-26 13:26:44 +0100

    first commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,10 @@
+import jobs from '../data/job-listing.json';
+import type { JobsList } from '../models';
+
+const job_list: JobsList = jobs;
+
+export const load = (() => {
+  return {
+    job_list
+  };
+})
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import JobDisplay from '../components/JobDisplay.svelte';
+  import type { PageServerData } from './$types';
+
+  export let data: PageServerData;
+</script>
+
+<div class="home-page">
+  <h1>Job Listing Home page</h1>
+
+  {#each data.job_list as job}
+    <JobDisplay job={job}/>
+  {/each}
+</div>
+  
+  <style>
+    .home-page {
+      padding: 2rem 4rem;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+    }
+  </style>
+  
